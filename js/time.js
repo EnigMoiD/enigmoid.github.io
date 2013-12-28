1190028658ad098be6b3d648b561fd15a7ae5832
EnigMoiD <evan.dorsky@icloud.com>
2013-12-28 01:37:45 -0500

    Home/about content, styl refactoring, time js good

--- js/time.js
+++ js/time.js
@@ -1,15 +1,25 @@
 (function TimeManager() {
 	Date.prototype.timeSince = function(date) {
-		var delta = date.getTime() - this.getTime();
+		var delta = this.getTime() - date.getTime()
 
-		var date = new Date(delta);
+		var date = new Date(delta)
 
 		return {
 			date: date,
+			seconds: date.getSeconds(),
+			minutes: date.getMinutes(),
+			hours: date.getHours(),
 			months: date.getMonth(),
 			days: date.getDate(),
 			years: date.getFullYear()-1970
 		}
+	}
+
+	function yearText(years, id) {
+		if (id != 'graduation')
+			return "<span class='number'>" + years + "</span> year" + ((years > 1)? "s" : "")
+
+		else return "<span class='number'>" + yearTitles[years] + "</span>";
 	}
 
 	var yearTitles = [
@@ -19,12 +29,20 @@
 		'Senior'
 	]
 
-	var now = new Date(),
-		birth = new Date(1994, 1, 26),
-		graduation = new Date(2016, 4, 20);
+	var now = new Date()
+	// Important dates
+	window.dates = {
+		birth: new Date(1994, 1, 26),
+		graduation: new Date(2016, 4, 20),
+		programming: new Date(2009, 10, 22),
+		violin: new Date(2004, 0, 1),
+		fire: new Date(2012, 10, 11)
+	}
 
-	var age = birth.timeSince(now).years;
+	var Evan = {}
+	for (var date in dates)
+		Evan[date] = Math.abs(dates[date].timeSince(now).years) - 1
 
-	var year = yearTitles[4+graduation.timeSince(now).years];
-	console.log(year);
+	for (var id in Evan)
+		document.getElementById(id).innerHTML = yearText(Evan[id], id)
 })()
