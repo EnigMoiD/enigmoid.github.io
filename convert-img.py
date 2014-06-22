3543abd2c2f679d2253a3820580909cc52defb91
Evan Dorsky <evan.dorsky@icloud.com>
2014-06-22 16:27:17 -0400

    Fix image conversion script

--- convert-img.py
+++ convert-img.py
@@ -1,12 +1,18 @@
 import sys, os, re
 from subprocess import call
 
-def resize(filename, dirname, width):
-	call(["convert", dirname+"/"+filename, "-resize", width, dirname+"/banner.jpg"])
+def resizeBanner(f, d):
+	resize(f, d, "900", "/banner.jpg")
+
+def resizeProject(f, d):
+	resize(f, d, "360", "/project.jpg")
+
+def resize(filename, dirname, width, new_name):
+	call(["convert", dirname+"/"+filename, "-resize", width, dirname+new_name])
 
 for dirname, dirnames, filenames in os.walk("."):
 	for filename in filenames:
 		if re.search(r"banner-full.jpg", filename):
-			resize(filename, dirname, "900")
+			resizeBanner(filename, dirname)
 		if re.search(r"project-full.jpg", filename):
-			resize(filename, dirname, "360")
+			resizeProject(filename, dirname)
