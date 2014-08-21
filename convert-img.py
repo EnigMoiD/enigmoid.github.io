d75de47b50fd028135a600e9d3fd6a572e982e60
Evan Dorsky <evan.dorsky@icloud.com>
2014-08-21 16:06:54 -0400

    Improve convert img script

--- convert-img.py
+++ convert-img.py
@@ -1,18 +1,36 @@
-import sys, os, re
+import sys, os
 from subprocess import call
 
 def resizeBanner(f, d):
-	resize(f, d, "900", "/banner.jpg")
+	return resize(f, d, "900", "banner.jpg")
 
 def resizeProject(f, d):
-	resize(f, d, "360", "/project.jpg")
+	return resize(f, d, "360", "project.jpg")
 
-def resize(filename, dirname, width, new_name):
-	call(["convert", dirname+"/"+filename, "-resize", width, dirname+new_name])
+def resize(filename, dirname, width, newname):
+	path = os.path.join(dirname, filename)
+	newpath = os.path.join(dirname, newname)
+	call(["convert", path, "-resize", width, newpath])
+	return path, newpath
 
-for dirname, dirnames, filenames in os.walk("."):
-	for filename in filenames:
-		if re.search(r"banner-full.jpg", filename):
-			resizeBanner(filename, dirname)
-		if re.search(r"project-full.jpg", filename):
-			resizeProject(filename, dirname)
+def colorize(dir, path):
+	call(["convert", path, "-grayscale", "rec709luma", "+level-colors", "'#000000','#2B3E53'", os.path.join(dir, "blue.jpg")])
+
+def resizefiles():
+	for dirname, dirnames, filenames in os.walk("./img"):
+		for filename in filenames:
+			if "banner-full.jpg" in filename:
+				path, newpath = resizeBanner(filename, dirname)
+			if "project-full.jpg" in filename:
+				resizeProject(filename, dirname)
+
+def colorizefiles():
+	for dirname, dirnames, filenames in os.walk("./img"):
+		for filename in filenames:
+			if "banner.jpg" in filename:
+				colorize(dirname, os.path.join(dirname, filename))
+
+### main
+
+resizefiles()
+colorizefiles()
