d541b313250ec1ded74bcaae66bb56b8eef506d2
Evan Dorsky <evan.dorsky@icloud.com>
2014-12-24 23:09:23 -0500

    Better banner transitions

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,27 +1,27 @@
 (function() {
-	var bgContainer, sizeBanner;
+	var bgContainer, posBanner;
 
 	bgContainer = $(".post-bg");
 
 	$(bgContainer).find("img").load(function() {
-		sizeBanner($($(this).parent()));
+		posBanner($($(this).parent()));
 	});
 
 	$(window).resize(function() {
 		bgContainer.each(function() {
-			sizeBanner($(this));
+			posBanner($(this));
 		});
 	});
 
-	sizeBanner = function(bgContainer) {
+	posBanner = function(bgContainer) {
 		var h = bgContainer.children().first().height();
 		var banner = bgContainer.parent();
 		var bannerPosition = bgContainer.attr("bannerPosition");
 
 		if (banner.attr("open") === "open")
-			banner.css("height", h)
-		else
-			bgContainer.css("margin-top", -bannerPosition * h)
+			return
+
+		bgContainer.css("margin-top", -bannerPosition * h)
 	};
 
 	window.oldOpenBanner = null
@@ -30,38 +30,32 @@
 
 	banner.click(function() {
 		var closeBanner = function(openBanner, newBanner, sameWasOpen) {
-			openBanner.animate({"height": "auto"}, 200)
 			openBanner.removeAttr("open")
 
 			var bgContainer = $(openBanner).find(".post-bg")
-			var height = bgContainer.height()
-			var bannerPosition = bgContainer.attr("bannerPosition")
-			bgContainer.animate({"margin-top": -bannerPosition * height}, 200)
+			posBanner(bgContainer)
 
 			var projContainer = $(openBanner).find(".proj-content").parent()
 			projContainer.removeClass("active")
-			projContainer.css({"background-color": "transparent"})
 
-			$(openBanner).find(".proj-content").animate({"height": "0px"}, 200, function() {
-				if (sameWasOpen) return
-				$('html, body').animate({
-					scrollTop: $(newBanner).offset().top
-				})
+			$(openBanner).find(".proj-content").css({"max-height": "0px"})
+
+			if (sameWasOpen) return
+			$('html, body').animate({
+				scrollTop: $(newBanner).offset().top
 			})
-
 		}
 
 		var openBanner = function(closedBanner, bannerOpen) {
-			var bgContainer = $(closedBanner).find(".post-bg")
-
-			closedBanner.animate({"max-height": bgContainer.find("img").height()}, 200)
 			closedBanner.attr("open", "true")
 			window.oldOpenBanner = closedBanner
 
-			bgContainer.animate({"margin-top": "0px"}, 200)
+			var bgImage = $(closedBanner).find(".post-bg").find("img")
+			var bgHeight = $(bgImage).height()
+			bgImage.parent().css("margin-top", "0px")
 
 			var projContainer = $(closedBanner).find(".proj-content").parent()
-			$(closedBanner).find(".proj-content").animate({"height": "100%"}, 200)
+			$(closedBanner).find(".proj-content").css({"max-height": bgHeight})
 			projContainer.addClass("active")
 
 			if (!bannerOpen) {
