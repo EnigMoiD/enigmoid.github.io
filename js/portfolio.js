ced4019725e92bd3ae3d7d8ad65c36f166ddd304
Evan Dorsky <evan.dorsky@icloud.com>
2014-12-27 00:41:13 -0500

    Remove unnecessary jQuery $() selections

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,7 +4,7 @@
 	bgContainer = $(".post-bg");
 
 	$(bgContainer).find("img").load(function() {
-		posBanner($($(this).parent()), false);
+		posBanner($(this).parent(), false);
 	});
 
 	$(window).resize(function() {
@@ -38,46 +38,46 @@
 
 	banners.click(function() {
 		var closeBanner = function(openBanner, newBanner) {
-			var bgContainer = $(openBanner).find(".post-bg")
-			var projContainer = $(openBanner).find(".proj-content").parent()
-			var bannerBanner = $(openBanner).find(".banner")
+			var bgContainer = openBanner.find(".post-bg")
+			var projContainer = openBanner.find(".proj-content").parent()
+			var bannerBanner = openBanner.find(".banner")
 			
 			// make it not modal before closing
 			openBanner.removeClass("open")
 			
 			// hide the content
-			$(openBanner).find(".proj-content").transition({"opacity": "0"}, 200)
+			openBanner.find(".proj-content").transition({"opacity": "0"}, 200)
 
 			// correct for scrolling that might have occurred
-			$(window).scrollTop($(openBanner).offset().top)
+			$(window).scrollTop(openBanner.offset().top)
 
 			// close the banner
-			$(openBanner).transition({"height": "auto"}, 200)
-			$(bannerBanner).transition({"height": "auto"}, 200)
+			openBanner.transition({"height": "auto"}, 200)
+			bannerBanner.transition({"height": "auto"}, 200)
 
 			// once it's closed, no longer active
 			openBanner.removeClass("active")
 		}
 
 		var openBanner = function(closedBanner, bannerOpen) {
-			var bgImage = $(closedBanner).find(".post-bg").find("img")
-			var bgHeight = $(bgImage).height()
-			var projContainer = $(closedBanner).find(".proj-content").parent()
-			var bannerBanner = $(closedBanner).find(".banner")
+			var bgImage = closedBanner.find(".post-bg").find("img")
+			var bgHeight = bgImage.height()
+			var projContainer = closedBanner.find(".proj-content").parent()
+			var bannerBanner = closedBanner.find(".banner")
 			
 			// make it active to style it before it opens
 			closedBanner.addClass("active")
 
 			// make the content visible and the right font size
-			$(closedBanner).find(".proj-content").transition({"opacity": 1}, 200)
+			closedBanner.find(".proj-content").transition({"opacity": 1}, 200)
 
 			// open the banner
-			$(closedBanner).transition({"height": $(window).height()}, 200)
-			$(bannerBanner).transition({"height": "15em"}, 200)
+			closedBanner.transition({"height": $(window).height()}, 200)
+			bannerBanner.transition({"height": "15em"}, 200)
 
 			// scroll the page to the top of the banner as it opens
 			$('html, body').animate({
-				scrollTop: $(closedBanner).offset().top
+				scrollTop: closedBanner.offset().top
 			}, 200, function() {
 				// make modal once the banner is open
 				closedBanner.addClass("open")
