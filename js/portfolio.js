32c1f4dde0d6cbb10d7df54581d2f19d75af8502
Evan Dorsky <evan.dorsky@icloud.com>
2015-01-02 13:33:35 -0500

    Support focus from url

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,27 +1,27 @@
 (function() {
-	var bgContainer, posBanner;
+	var bgContainer, posBanner
 
-	bgContainer = $(".post-bg");
+	bgContainer = $(".post-bg")
 
 	$(bgContainer).find("img").load(function() {
-		posBanner($(this).parent(), false);
-	});
+		posBanner($(this).parent(), false)
+	})
 
 	$(window).resize(function() {
 		bgContainer.each(function() {
-			posBanner($(this), false);
-		});
-	});
+			posBanner($(this), false)
+		})
+	})
 
-	isOpenBanner = function() {
+	var isOpenBanner = function() {
 		return $(".open")
 	}
 
-	posBanner = function(bgContainer, transition) {
-		var banner = bgContainer.parent();
-		var bannerPosition = bgContainer.attr("bannerPosition");
+	var posBanner = function(bgContainer, transition) {
+		var banner = bgContainer.parent()
+		var bannerPosition = bgContainer.attr("bannerPosition")
 		var bgImg = bgContainer.find("img")
-		var h = bgImg.height();
+		var h = bgImg.height()
 
 		if (banner.hasClass("open"))
 			return
@@ -29,68 +29,73 @@
 			bgImg.transition({"margin-top": -bannerPosition * h}, 200)
 		else
 			bgImg.css("margin-top", -bannerPosition * h)
-	};
-
-
-	var banners = $('.project.snippet')
+	}
 
 	window.oldOpenBannerHeight = null
 
+	var closeBanner = function(openBanner, newBanner) {
+		var bgContainer = openBanner.find(".post-bg")
+		var projContainer = openBanner.find(".proj-content").parent()
+		var bannerBanner = openBanner.find(".banner")
+		var bContent = openBanner.find(".banner-content")
+		
+		// make it not modal before closing
+		openBanner.removeClass("open")
+		
+		// hide the content
+		openBanner.find(".proj-content").animate({"opacity": "0"}, 200)
+
+		// correct for scrolling that might have occurred
+		$(window).scrollTop(openBanner.offset().top)
+
+		// close the banner
+		bannerBanner.animate({"height": oldOpenBannerHeight}, 200, function() {
+			bannerBanner.css({"height": "auto"})
+		})
+		openBanner.animate({"height": oldOpenBannerHeight}, 200, function() {
+			openBanner.css({"height": "auto"})
+		})
+
+		// once it's closed, no longer active
+		openBanner.removeClass("active")
+	}
+
+	var openBanner = function(closedBanner, bannerOpen) {
+		var bgImage = closedBanner.find(".post-bg").find("img")
+		var bgHeight = bgImage.height()
+		var projContainer = closedBanner.find(".proj-content").parent()
+		var bannerBanner = closedBanner.find(".banner")
+		
+		// make it active to style it before it opens
+		closedBanner.addClass("active")
+
+		// make the content visible and the right font size
+		closedBanner.find(".proj-content").animate({"opacity": 1}, 200)
+
+		// open the banner
+		closedBanner.animate({"height": $(window).height()}, 200)
+		bannerBanner.animate({"height": "10em"}, 200)
+
+		// scroll the page to the top of the banner as it opens
+		$('html, body').animate({
+			scrollTop: closedBanner.offset().top
+		}, 200, function() {
+			// make modal once the banner is open
+			closedBanner.addClass("open")
+		})
+
+		window.oldOpenBannerHeight = closedBanner.height()
+	}
+
+	// focusing targeted banner
+	var focusId = window.location.href.split("#")[1]
+	if (focusId)
+		openBanner($("#"+focusId), false)
+
+	// banner click handling
+	var banners = $('.project.snippet')
+
 	banners.click(function() {
-		var closeBanner = function(openBanner, newBanner) {
-			var bgContainer = openBanner.find(".post-bg")
-			var projContainer = openBanner.find(".proj-content").parent()
-			var bannerBanner = openBanner.find(".banner")
-			var bContent = openBanner.find(".banner-content")
-			
-			// make it not modal before closing
-			openBanner.removeClass("open")
-			
-			// hide the content
-			openBanner.find(".proj-content").animate({"opacity": "0"}, 200)
-
-			// correct for scrolling that might have occurred
-			$(window).scrollTop(openBanner.offset().top)
-
-			// close the banner
-			bannerBanner.animate({"height": oldOpenBannerHeight}, 200, function() {
-				bannerBanner.css({"height": "auto"})
-			})
-			openBanner.animate({"height": oldOpenBannerHeight}, 200, function() {
-				openBanner.css({"height": "auto"})
-			})
-
-			// once it's closed, no longer active
-			openBanner.removeClass("active")
-		}
-
-		var openBanner = function(closedBanner, bannerOpen) {
-			var bgImage = closedBanner.find(".post-bg").find("img")
-			var bgHeight = bgImage.height()
-			var projContainer = closedBanner.find(".proj-content").parent()
-			var bannerBanner = closedBanner.find(".banner")
-			
-			// make it active to style it before it opens
-			closedBanner.addClass("active")
-
-			// make the content visible and the right font size
-			closedBanner.find(".proj-content").animate({"opacity": 1}, 200)
-
-			// open the banner
-			closedBanner.animate({"height": $(window).height()}, 200)
-			bannerBanner.animate({"height": "10em"}, 200)
-
-			// scroll the page to the top of the banner as it opens
-			$('html, body').animate({
-				scrollTop: closedBanner.offset().top
-			}, 200, function() {
-				// make modal once the banner is open
-				closedBanner.addClass("open")
-			})
-
-			window.oldOpenBannerHeight = closedBanner.height()
-		}
-
 		var selfWasOpen = $(this).hasClass("open")
 
 		// close any open banners (there should only be one)
@@ -102,4 +107,4 @@
 
 		openBanner($(this), theOpenBanner.length > 0? true : false)
 	})
-})();
+})()
